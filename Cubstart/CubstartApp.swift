ed51e07977e7c960658483399ebad985586d1ea7
vivi0275 <victorpicart02@gmail.com>
2025-11-17 20:00:43 -0800

    Initial Commit

---
+++ Cubstart/CubstartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubstartApp.swift
+//  Cubstart
+//
+//  Created by victor picart on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CubstartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CubstartTests/CubstartTests.swift
@@ -0,0 +1,16 @@
+//
+//  CubstartTests.swift
+//  CubstartTests
+//
+//  Created by victor picart on 17/11/2025.
+//
+
+import Testing
+
+struct CubstartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
